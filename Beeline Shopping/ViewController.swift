4a115781e9efb27c2ee956e53614bdf4b077b6c2
Vilyan01 <bheller2010@live.com>
2016-01-08 13:41:21 -0800

    Initial Commit

---
+++ Beeline Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beeline Shopping
+//
+//  Created by Brian Heller on 1/8/16.
+//  Copyright © 2016 Beeline Shopping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
